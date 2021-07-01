d9038b99fe8fa6e6cc906c42bc3d960b4b1b138e
John Uzyanov <37864545+bigtelescope@users.noreply.github.com>
2021-07-01 20:09:25 +0300

    task #1

---
+++ Andrey/Classes/1.07/dividers.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> get_all_dividers(int a)
+{
+	vector <int> dividers;
+	int b = 1;
+	for (b; b != a + 1; b = b + 1)
+	{
+		if (a % b == 0)
+		{
+			dividers.push_back(b);
+		}
+
+	}
+	return dividers;
+}
+
+int main()
+{
+	int number_1 = 14;
+	int number_2 = 134;
+	int number_3 = 71;
+
+	vector<int> result_1 = get_all_dividers(number_1);
+	vector<int> result_2 = get_all_dividers(number_2);
+	vector<int> result_3 = get_all_dividers(number_3);
+
+	for (int i = 0; i < result_1.size(); i++)
+	{
+		cout << result_1[i] << endl;
+	}
+
+	for (int i = 0; i < result_2.size(); i++)
+	{
+		cout << result_2[i] << endl;
+	}
+
+	for (int i = 0; i < result_3.size(); i++)
+	{
+		cout << result_3[i] << endl;
+	}
+}
---
+++ Andrey/Classes/1.07/the biggest common divider.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int get_the_biggest_element(vector<int> c)
+{
+	int x = c[0];
+	for (int i = 0; i < c.size(); i++)
+	{
+		if (x < c[i])
+			x = c[i];
+	}
+	return x;
+}
+
+int get_the_biggest_common_divider(int a, int b)
+{
+	// находим максимальный делитель Х первого числа
+	vector<int> a_dividers = get_all_dividers(a);
+	temp = get_the_biggest_element(a_dividers);
+
+	// тут будет цикл
+
+	// если Х есть делитель числа b, то задача решена
+	if (b % temp == 0)
+		return temp;
+
+	// если Х не делитель числа b, то надо удалить Х из a_dividers и найти новый Х
+	// делаем все то же самое, то есть строку 24
+}
+
+vector<int> get_all_dividers(int a)
+{
+	vector <int> dividers;
+	int b = 1;
+	for (b; b != a + 1; b = b + 1)
+	{
+		if (a % b == 0)
+		{
+			dividers.push_back(b);
+		}
+
+	}
+	return dividers;
+}
+
+int main()
+{
+	int a = 64;
+	int b = 72;
+	cout << get_the_biggest_common_divider(a, b);
+}
