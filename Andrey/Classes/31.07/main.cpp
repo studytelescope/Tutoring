054128dcfed3d9b24dc40bcec4d8df8fa7fa0078
John Uzyanov <37864545+bigtelescope@users.noreply.github.com>
2021-07-31 13:05:28 +0300

    Create main.cpp

---
+++ Andrey/Classes/31.07/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string short_string = "";
+    string long_string = "AAAABBBDEHHHHHHHHHHHHHHHHH";
+
+    // your code
+    
+    cout << short_string << endl; // A4B3D1E1H17
+
+    return 0;
+}
