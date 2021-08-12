72805b9175a5156a915b68b9332232171ed6cb0f
John Uzyanov <37864545+bigtelescope@users.noreply.github.com>
2021-08-12 18:52:43 +0300

    Create delete_k_char.cpp
    rare version

---
+++ Andrey/Classes/12.08/delete_k_char.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string delete_k_symbol(string s, int index)
+{
+    // your code
+}
+
+int main()
+{
+    string base_string = "hello, world!";
+    cout << delete_k_symbol(base_string, 3) << endl;
+    return 0;
+}
