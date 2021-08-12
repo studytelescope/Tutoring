059c34e48b9095a45c762ef3eb211d10edcf615f
John Uzyanov <37864545+bigtelescope@users.noreply.github.com>
2021-08-12 18:56:17 +0300

    Create numbers_in_a_row.cpp

---
+++ Andrey/Classes/12.08/numbers_in_a_row.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string delete_numbers_in_a_row(string s)
+{
+    // your code
+}
+
+int main()
+{
+    string text = "abcd123def32ghried5739h48512312kopl";
+    cout << delete_numbers_in_a_row(text) << endl;
+    return 0;
+}
